76fb77ce2198399970c2bb2798006154086bbff0
kishan <kspatel2018@gmail.com>
2016-02-25 14:12:11 -0500

    add Django login authentication

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from .models import Review, Wine
 from .forms import ReviewForm
 import datetime
@@ -28,7 +29,7 @@ def wine_detail(request, wine_id):
     form = ReviewForm()
     return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
 
-
+@login_required
 def add_review(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     form = ReviewForm(request.POST)
@@ -36,6 +37,7 @@ def add_review(request, wine_id):
         rating = form.cleaned_data['rating']
         comment = form.cleaned_data['comment']
         user_name = form.cleaned_data['user_name']
+        user_name = request.user.username
         review = Review()
         review.wine = wine
         review.user_name = user_name
@@ -49,3 +51,18 @@ def add_review(request, wine_id):
         return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
 
     return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
+
+
+def user_review_list(request, username=None):
+    if not username:
+        username = request.user.username
+    latest_review_list = Review.objects.filter(user_name=username).order_by('-pub_date')
+    context = {'latest_review_list':latest_review_list, 'username':username}
+    return render(request, 'reviews/user_review_list.html', context)
+
+
+
+
+
+
+
